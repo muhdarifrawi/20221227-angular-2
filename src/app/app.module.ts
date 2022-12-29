5b007dc0f014ee9622dadf0dfacd180c728c1c86
muhdarifrawi <muhdarifrawi@gmail.com>
2022-12-29 14:11:39 +0800

    Added the following items: - New compoenents - New stylsheet - Installed Material UI - Added buttons from Material - Added Angular conditional rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewoneComponent } from './newone/newone.component';
+import { NewtwoComponent } from './newtwo/newtwo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewoneComponent,
+    NewtwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/newone/newone.component.ts
@@ -0,0 +1,12 @@
+import { Component, NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'app-newone',
+  templateUrl: './newone.component.html',
+  // template:'<h1>hello this is the template</h1>',
+  styleUrls: ['./newone.component.css','./style.css']
+})
+export class NewoneComponent {
+  val=false
+}
