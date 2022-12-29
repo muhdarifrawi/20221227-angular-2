08c212aa69e5f8921682e85f8c46d1dd499d1472
muhdarifrawi <muhdarifrawi@gmail.com>
2022-12-29 17:14:13 +0800

    20221229 Added the following stuff: - for-loop - built-in directives/bindings

--- src/app/newone/newone.component.ts
+++ src/app/newone/newone.component.ts
@@ -9,4 +9,55 @@ import { Component, NgModule } from '@angular/core';
 })
 export class NewoneComponent {
   val=false
+  obj =[
+    {
+      number: 1,
+      name: "apple",
+      age:21
+    },
+    {
+      number: 2,
+      name: "banana",
+      age:24
+    },
+    {
+      number: 3,
+      name: "cashew",
+      age:27
+    },
+    {
+      number: 4,
+      name: "durian",
+      age:23
+    },
+  ]
+  game="rugby"
+  c="violet"
+  bg="purple"
+  fw=100
+  colorVal=2
+  classVal = "a b"
+  classA = "a"
+  classB="b"
+  classCompare = 1
+
+
+  classType(){
+    return "a"
+  }
+
+  headerRow="This is a title for the table"
+
+  imgSrc=""
+  imgAlt="A picture of a cat"
+
+  colSpan=3
+
+  actionCall(){
+    alert("Hey it works!")
+  }
+
+  withParams(a:any, b:any, c:any){
+    alert("here are the results:" + a +", "+ b +", "+ c)
+  }
 }
